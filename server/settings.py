3cb335b4a7ef29ebef2308cc46ebbf5956e345e6
sajadkh <sajadkhosravi1996@gmail.com>
2019-02-03 21:43:03 +0330

    add alowed host

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ieua(+^)(#-5u57hi150hj50!tr0gn$+254t%u)w*91k*d#c3i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
